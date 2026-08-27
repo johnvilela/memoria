8d32d4581d4d2ce743ad93aecc43fed237e465e9
John Vilela <johnvvilela@gmail.com>
2026-08-26 21:47:00 -0300

    chore(release): bump version to 0.8.0

--- cmd/memoria/main.go
+++ cmd/memoria/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 // version is bumped by hand on release, together with the matching git tag.
-const version = "0.7.0"
+const version = "0.8.0"
 
 func run(args []string, stdin io.Reader, out io.Writer) int {
 	arg := ""
